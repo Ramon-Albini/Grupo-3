9abe15b65ccfc215aa433296815948088ceacab2
Ramon-Albini <ramonalbini13@gmail.com>
2025-06-15 00:09:57 -0300

    adding the models with all the relationships with each other

---
+++ Nota.Projeto/src/main/java/com/Facil/Nota/Projeto/Models/Student.java
@@ -0,0 +1,45 @@
+package com.Facil.Nota.Projeto.Models;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")//entidade ESTUDANTE	
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	@Column(nullable = false)
+	String name;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "student_course",
+			joinColumns = @JoinColumn(name = "student_id"),
+			inverseJoinColumns = @JoinColumn(name = "course_id"))
+	List<Course> coursesIds;
+	@OneToMany(mappedBy = "studentId")
+	List<Grade> gradesIds;
+	@OneToMany(mappedBy = "studentId")
+	List<Absence> absencesIds;
+	
+}
