210415c04de1f677d90bb659b3ee8955d4115a7c
Ramon Albini <ramonalbini13@gmail.com>
2025-06-18 22:54:14 -0300

    Fix spring logic

--- Nota.Projeto/src/main/java/com/Facil/Nota/Projeto/Services/UserService.java
+++ Nota.Projeto/src/main/java/com/Facil/Nota/Projeto/Services/UserService.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 
 import com.Facil.Nota.Projeto.DTOs.UserDTO;
 import com.Facil.Nota.Projeto.Exceptions.NotFoundException;
+import com.Facil.Nota.Projeto.Models.Student;
 import com.Facil.Nota.Projeto.Models.User;
+import com.Facil.Nota.Projeto.Repositories.StudentRepository;
 import com.Facil.Nota.Projeto.Repositories.UserRepository;
 
 import jakarta.transaction.Transactional;
@@ -14,15 +16,23 @@ import jakarta.transaction.Transactional;
 @Service
 public class UserService {
 	private final UserRepository userRepository;
+	private final StudentRepository studentRepository;
 
 	
-	public UserService(UserRepository userRepository) {
+	public UserService(UserRepository userRepository, StudentRepository studentRepository) {
 		this.userRepository = userRepository;
+		this.studentRepository = studentRepository;
 	}
 	
 	public UserDTO saveUser(UserDTO userDTO) {
 		
 		User userEntity = new User(userDTO);
+		
+		if (userDTO.studentId() != null ) {
+			Student student = studentRepository.findById(userDTO.studentId())
+					.orElseThrow(() -> new NotFoundException("Student not found"));
+			userEntity.setStudent(student);
+		}	
 		
 		return new UserDTO(userRepository.save(userEntity));
 	}
